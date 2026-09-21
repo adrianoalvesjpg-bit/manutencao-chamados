5f295b625bf2303566e83d038d6eee2a95a056ae
Adriano Alves <adrianoalves.jpg@gmail.com>
2026-09-21 12:47:01 -0300

    Iniciado card dos chamados, ainda sem ser um componente para ser utilizado em lista.

--- front/src/components/Main.jsx
+++ front/src/components/Main.jsx
@@ -1,9 +1,33 @@
 import {useState} from 'react'
 
+import StMain from '../style/Main.module.css'
+
 export default function Main() {
   return (
-    <div>
-      
+    <div className={StMain.container}>
+      <div className={StMain.card} id={StMain.open}>
+        <div className={StMain.titleDiv}>
+          <h1>Manutenção de Equipamento - MANEQP-0001</h1>
+        </div>
+
+        <div className={StMain.main}>
+          <div className={StMain.item}>
+            <p>Solicitante:</p>
+            <span>Adriano Alves Vieira</span>
+          </div>
+
+          <div className={StMain.item}>
+            <p>Setor:</p>
+            <span>Engenharia</span>
+          </div>
+
+          <div className={StMain.item}>
+            <p>Código do Equipamento:</p>
+            <span>9252325</span>
+          </div>
+        </div>
+
+      </div>
     </div>
   )
 }
